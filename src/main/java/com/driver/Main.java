701ced0a4b9caaf4ead998cd875e19adee204f45
neyhere07 <nyananayak27.com>
2024-03-21 12:22:35 +0530

    commit done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
+// Task 1
+class A {
+    public String meth() {
+        return "Invoking method from class A";
+    }
+}
+
+// Task 2
+class B extends A {
+    // Task 4
+    @Override
+    public String meth() {
+        return "Method is overridden in Extended class B";
+    }
+}
+
 public class Main {
-  
+    public static void main(String[] args) {
+        // Task 3
+        B b = new B();
+        System.out.println(b.meth());
+    }
 }
