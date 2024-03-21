be3921ae1047f7ee42d5785290157473cee8120f
neyhere07 <nyananayak27.com>
2024-03-21 12:52:57 +0530

    commit here

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,25 +1,20 @@
 package com.driver;
 
-// Task 1
-class A {
-    public String meth() {
-        return "Invoking method from class A";
-    }
-}
-
-// Task 2
-class B extends A {
-    // Task 4
-    @Override
-    public String meth() {
-        return "Method is overridden in Extended class B";
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
-        // Task 3
-        B b = new B();
-        System.out.println(b.meth());
+        B obj = new B();
+        System.out.println(obj.meth());
     }
+    public static class A{
+            public String meth(){
+                return "Invoking method from class A";
+            }
+        }
+
+        public static class B extends A{
+            @Override
+            public String meth(){
+                return "Method is overridden in Extendend class B";
+            }
+        }
 }
